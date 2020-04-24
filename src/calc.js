84283a6e68c88be3aa178d137fd725569684e59f
sandrabosk <sandraboskovic@hotmail.com>
2020-04-24 11:36:00 -0400

    minor update/alternative approach

--- src/calc.js
+++ src/calc.js
@@ -3,6 +3,7 @@ function sum(array) {
     throw new Error('No parameter provided');
   }
 
+  // if (!Array.isArray(arr)) return 0;
   if (array.length === 0) return 0;
 
   return array.reduce((acc, value) => acc + value, 0);
