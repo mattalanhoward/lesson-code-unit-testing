ae9fa266113fa6f234a88fe1bbd481c2f49473c6
Matthew Howard <mattalanhoward@gmail.com>
2020-06-27 15:19:00 +0200

    exercise done

--- spec/calc.spec.js
+++ spec/calc.spec.js
@@ -26,3 +26,34 @@ describe('The function sum() used in the Calculator', () => {
     expect(sum([1, 2, 3, 4, 5])).toBe(15);
   });
 });
+
+
+//make tests for avg function.
+describe('The function avg() used in the Calculator', () => {
+  it('should be a function', () => {
+    expect(typeof avg).toBe('function');
+  });
+
+  it('should return 0 for an empty array', () => {
+    expect(avg([])).toBe(0);
+  });
+
+  it('should throw an error if no parameter is provided', () => {
+    expect(() => {
+      avg();
+    }).toThrow(new Error('No parameter provided'));
+  });
+
+  it('should return a number', () => {
+    expect(typeof avg([1, 1])).toBe('number');
+  });
+
+  it('should return the correct value', () => {
+    // if array with one element is passed, sum should be equal to  that element
+    expect(avg([1])).toBe(1);
+    expect(avg([1, 3])).toEqual(2);
+    expect(avg([4, 5, 6])).toBe(5);
+    expect(avg([10,30])).toBe(20);
+    expect(avg([1, 2, 3, 4, 5])).toBe(3);
+  });
+});
--- src/calc.js
+++ src/calc.js
@@ -8,3 +8,22 @@ function sum(array) {
 
   return array.reduce((acc, value) => acc + value, 0);
 }
+
+function avg(array) {
+  if (array === undefined) {
+    throw new Error('No parameter provided');
+  }
+
+  // if (!Array.isArray(arr)) return 0;
+  if (array.length === 0) return 0;
+
+  var total = 0;
+  for(var i = 0; i < array.length; i++) {
+      total += array[i];
+  }
+  var avg = total / array.length;
+
+  return avg;
+}
+
+
