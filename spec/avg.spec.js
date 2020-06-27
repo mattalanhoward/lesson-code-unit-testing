52679779e5d213786be76637d9693a134b790d74
Matthew Howard <mattalanhoward@gmail.com>
2020-06-27 15:57:37 +0200

    separated functions/code into modules

---
+++ spec/avg.spec.js
@@ -0,0 +1,17 @@
+function avg(array) {
+    if (array === undefined) {
+      throw new Error('No parameter provided');
+    }
+  
+    // if (!Array.isArray(arr)) return 0;
+    if (array.length === 0) return 0;
+  
+    var total = 0;
+    for(var i = 0; i < array.length; i++) {
+        total += array[i];
+    }
+    var avg = total / array.length;
+  
+    return avg;
+  }
+  
